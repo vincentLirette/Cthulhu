e62fe339fbfdeeca4b0a17dd509058f8667ad114
vincentLirette <vi.lirette@gmail.com>
2017-03-19 16:42:54 -0400

    Add new !info command.

--- README.md
+++ README.md
@@ -6,17 +6,13 @@
 
 List the available commands.
 
+### !info
+
+Show the info about the community.
+
 ### !cat
 
 Cats are awesome.
-
-### !tebeste
-
-Google memes. You know.
-
-### !ping
-
-Use this command to check if Cthulhu is awake.
 
 ### !flip
 
@@ -25,6 +21,14 @@ You want to flip a coin, but the other person is far away? Use this.
 ### !roll
 
 Basically like rolling a 6-sided dice.
+
+### !ping
+
+Use this command to check if Cthulhu is awake.
+
+### !tebeste
+
+Google memes. You know.
 
 ### !rhum
 
--- src/modules/commands/commands.rb
+++ src/modules/commands/commands.rb
@@ -1,27 +1,26 @@
 module Bot
   module DiscordCommands
-    # Command for evaluating Ruby code in an active bot.
-    # Only the `event.user` with matching discord ID of `CONFIG.owner`
-    # can use this command.
     module Commands
       extend Discordrb::Commands::CommandContainer
       command :commands do |event|
         "```
 These are the available commands:
 
+!info - Show the info about the community.
+
 !cat - Cats are awesome.
 
 !flip - You want to flip a coin, but the other person is far away? Use this.
 
-!skippy - Rhum is life.
-
-!tree - You will see.
-
-!tebeste - Google memes, you know?
-
 !roll - Basically like rolling a 6-sided dice.
 
 !ping - Use this command to check if the Cthulhu is awake.
+
+!tebeste - Google memes, you know?
+
+!rhum - Rhum is life.
+
+!tree - You will see.
 
 !teabag - Oh yeah.
         ```"
