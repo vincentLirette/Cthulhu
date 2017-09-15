1a1a2a3784a015648a65d1610db72a286e8a3ac2
Vincent <vi.lirette@gmail.com>
2017-09-15 10:57:41 -0400

    Test description in command.

--- src/modules/commands/cat.rb
+++ src/modules/commands/cat.rb
@@ -2,8 +2,9 @@ module Bot
   module DiscordCommands
     module Cat
       extend Discordrb::Commands::CommandContainer
+      cat_desc = "Cats are awesome"
       bucket :cats, limit: 3, time_span: 60, delay: 10
-      command(:cat, bucket: :cats) do |event|
+      command(:cat, description: cat_desc, bucket: :cats) do |event|
 	uri = URI('http://random.cat/meow')
         json = JSON.parse(Net::HTTP.get(uri))
         event.respond json['file']
