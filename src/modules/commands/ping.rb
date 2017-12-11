3067d09af39cb40b382f465b8a8aa2edbaa7ceb1
vincentLirette <vi.lirette@gmail.com>
2017-12-11 18:17:42 -0500

    Update command

--- src/modules/commands/ping.rb
+++ src/modules/commands/ping.rb
@@ -2,9 +2,11 @@ module Bot
   module DiscordCommands
     # Responds with "Pong!".
     # This used to check if bot is alive
-    module Ping
-      extend Discordrb::Commands::CommandContainer
-      command(:ping ) { |event| "`#{Time.now - event.timestamp}ms`" }
+    module Ping extend Discordrb::Commands::CommandContainer
+      command(:ping,
+              help_available: false) do |event|
+        "`#{Time.now - event.timestamp}ms`"
+      end
     end
   end
 end
