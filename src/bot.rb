d347efbce7aa5ca6bc9570a480062027522240a5
vincentLirette <vi.lirette@gmail.com>
2017-12-11 17:48:07 -0500

    Add command_doesnt_exist_message

--- src/bot.rb
+++ src/bot.rb
@@ -16,7 +16,8 @@ module Bot
   # can access the cache anywhere.
   BOT = Discordrb::Commands::CommandBot.new(client_id: CONFIG.client_id,
                                             token: CONFIG.token,
-                                            prefix: CONFIG.prefix)
+                                            prefix: CONFIG.prefix,
+                                            command_doesnt_exist_message: CONFIG.command_doesnt_exist_message)
 
   # Discord commands
   module DiscordCommands; end
