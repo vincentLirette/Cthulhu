06942b52d963d18faa9d1550a4dc03ca1af9d6e0
vincentLirette <vi.lirette@gmail.com>
2017-12-11 17:51:44 -0500

    Update !info

--- src/modules/commands/info.rb
+++ src/modules/commands/info.rb
@@ -4,11 +4,8 @@ module Bot
       extend Discordrb::Commands::CommandContainer
       command :info do |event|
         "```
-CNF-Paragon
-Communauté Nord-Americaine Francophone de Paragon
-
 Bienvenue !
-CNF-Paragon est, comme en indique le nom, la plus grosse communauté de joueurs, d'équipes, de streamers et de youtubers qui parlent français et qui jouent sur les serveurs NA du jeu Paragon. Cette communauté est ouverte à tous les francophones  sur NA  et il nous fera plaisirs de vous voir participer à la vie communautaire du groupe. N'oubliez pas de nous rejoindre via nos divers plat-formes !
+CNF: Communauté Nord-Americaine Francophone
         ```"
       end
     end
