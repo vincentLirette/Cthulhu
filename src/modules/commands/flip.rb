ef72296a80039af80f90a4464ca45999909fdf05
vincentLirette <vi.lirette@gmail.com>
2017-12-11 18:12:35 -0500

    Update help + description + usage

--- src/modules/commands/flip.rb
+++ src/modules/commands/flip.rb
@@ -1,15 +1,11 @@
 module Bot
   module DiscordCommands
-    # Command for evaluating Ruby code in an active bot.
-    # Only the `event.user` with matching discord ID of `CONFIG.owner`
-    # can use this command.
-    module Flip
-      extend Discordrb::Commands::CommandContainer
+    module Flip extend Discordrb::Commands::CommandContainer
       desc = "Flip a coin."
-      command (:flip,
-        description: desc,
-        help_available: true
-        usage: "#{BOT.prefix}flip") do |event|
+      command(:flip,
+              description: desc,
+              usage: "#{BOT.prefix}flip",
+              help_available: true ) do |event|
 
         coin = ["Heads", "Tail"]
         coin.sample
--- src/modules/commands/roll.rb
+++ src/modules/commands/roll.rb
@@ -1,14 +1,15 @@
 module Bot
   module DiscordCommands
-    # Command for evaluating Ruby code in an active bot.
-    # Only the `event.user` with matching discord ID of `CONFIG.owner`
-    # can use this command.
-    module Roll
-      extend Discordrb::Commands::CommandContainer
-      command :roll do |event, min = 1, max = 5|
-        number = Random.new
-        number.rand(min.to_i..max.to_i)
-      end
+    module Roll extend Discordrb::Commands::CommandContainer
+      desc = "Rolling a dice. Default is a 6-sided dice. You can change the min and max values"
+      command(:roll,
+                description: desc,
+                usage: "#{BOT.prefix}roll\n#{BOT.prefix}roll <min> <max>",
+                help_available: true) do |event, min = 1, max = 5|
+        random = Random.new
+        number = random.rand(min.to_i..max.to_i)
+        event << number
+      end #command
     end
   end
 end
